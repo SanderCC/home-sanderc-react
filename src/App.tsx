2759f726995bcdbd23c7a0b61bff8e103ceba252
Sander Constantin <sander.constantin@student.kdg.be>
2024-07-10 20:36:33 +0200

    Latest

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import '@fontsource/inter';
 import {Box, Card, Stack} from "@mui/joy";
+import Portrait from "./components/Portrait";
+import GitHubStats from "./components/GitHubStats";
+import ProfileButtons from "./components/ProfileButtons";
 import "./css/app.css"
-import SiteButton from "./components/SiteButton";
 
 function App() {
   return (
@@ -10,32 +12,13 @@ function App() {
           <Card>
               <Stack direction={"row"} spacing={5} sx={{p:3}}>
                   <div>
-                      <img src={"https://avatars.githubusercontent.com/u/58855319?v=4"}
-                           height={200}
-                           style={{borderRadius: 10}}
-                           alt={"Sander Constantin"} />
+                      <Portrait />
                   </div>
                   <div>
-                      <img src={"https://github-readme-stats.vercel.app/api" +
-                          "?username=SanderCC" +
-                          "&show_icons=true" +
-                          "&theme=transparent" +
-                          "&title_color=000" +
-                          "&icon_color=000" +
-                          "&text_color=000" +
-                          "&hide_border=true"}
-                            alt={"GitHub stats"}
-                      />
-                      <Stack direction={"row"} spacing={2}>
-                          <SiteButton
-                              size={30}
-                              icon={"https://github.githubassets.com/favicons/favicon.png"}
-                              url={"https://github.com/SanderCC"} />
-                          <SiteButton
-                              size={31}
-                              icon={"https://static.licdn.com/aero-v1/sc/h/akt4ae504epesldzj74dzred8"}
-                              url={"https://www.linkedin.com/in/sanderconstantin/"} />
-                      </Stack>
+                      <GitHubStats />
+                      <Box display={"flex"} justifyContent={"center"}>
+                          <ProfileButtons />
+                      </Box>
                   </div>
               </Stack>
           </Card>
