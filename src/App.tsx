e76279f4251d52135bc2982af1dee00f56fe277e
Sander Constantin <sconstantin@3p.eu>
2024-07-10 16:07:22 +0200

    Beginning

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,44 @@
 import React from 'react';
 import '@fontsource/inter';
-import {Stack} from "@mui/joy";
+import {Box, Card, Stack, Typography} from "@mui/joy";
 
 function App() {
   return (
-      <Stack direction="row" spacing={1} flexWrap="wrap" useFlexGap>
-          <div>Item 1</div>
-          <div>Item 2</div>
-          <div>Item 3</div>
-      </Stack>
+      <Box display={"flex"} justifyContent={"center"} alignItems={"center"} sx={{m:0,p:0, height: "95vh"}}>
+          <Card>
+              <Stack direction={"row"} spacing={5} sx={{p:3}}>
+                  <div>
+                      <img src={"https://avatars.githubusercontent.com/u/58855319?v=4"}
+                           height={200}
+                           style={{borderRadius: 10}}
+                           alt={"Sander Constantin"} />
+                  </div>
+                  <div>
+                      <img src={"https://github-readme-stats.vercel.app/api" +
+                          "?username=SanderCC" +
+                          "&show_icons=true" +
+                          "&theme=transparent" +
+                          "&title_color=000" +
+                          "&icon_color=000" +
+                          "&text_color=000" +
+                          "&hide_border=true"}
+                            alt={"GitHub stats"}
+                      />
+                      <Stack direction={"row"} spacing={3}>
+                          <div>
+                              O
+                          </div>
+                          <div>
+                              O
+                          </div>
+                          <div>
+                              O
+                          </div>
+                      </Stack>
+                  </div>
+              </Stack>
+          </Card>
+      </Box>
   );
 }
 
