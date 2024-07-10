dc178ea779d3733c0148bf6ec56fc3f9c0bfdcb7
Sander Constantin <sander.constantin@student.kdg.be>
2024-07-10 20:53:48 +0200

    simplify

--- src/components/SiteButton.tsx
+++ src/components/SiteButton.tsx
@@ -1,8 +1,9 @@
 export default function SiteButton({icon, url, size}:{icon:string,url:string, size:number}) {
-    return <img onClick={() => window.open(url, "_blank")}
-                src={icon}
-                className={"iconButton"}
-                height={size}
-                width={size}
-                alt={"Site button"} />
+    return <a href={url} target={"_blank"} rel="noreferrer">
+        <img src={icon}
+             className={"iconButton"}
+             height={size}
+             width={size}
+             alt={"Site button"} />
+    </a>
 }
