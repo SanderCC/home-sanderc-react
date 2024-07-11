c5336eab8881a3e2cc0319f425c0ef24c341d2e4
Sander Constantin <58855319+SanderCC@users.noreply.github.com>
2024-07-11 13:21:57 +0200

    Update ProfileButtons.tsx

--- src/components/ProfileButtons.tsx
+++ src/components/ProfileButtons.tsx
@@ -12,5 +12,9 @@ export default function ProfileButtons() {
             size={31}
             icon={"https://static.licdn.com/aero-v1/sc/h/akt4ae504epesldzj74dzred8"}
             url={"https://www.linkedin.com/in/sanderconstantin/"} />
+        <SiteButton
+            size={30}
+            icon={"https://www.paypalobjects.com/webstatic/icon/pp32.png"}
+            url={"https://paypal.me/SanderC"} />
     </Stack>
 }
